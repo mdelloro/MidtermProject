6535d7394a1eaa648c35a6793d72e0a3b5c34084
Jhobson313 <james-hobson@att.net>
2016-09-16 21:41:07 -0400

    new action gui

--- src/midterm/Grid.java
+++ src/midterm/Grid.java
@@ -1,5 +1,8 @@
 package midterm;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,17 +15,30 @@ public class Grid {
 		
 		JFrame frame = new JFrame("Bombermen Minesweeper Game");
 		frame.setVisible(true);
-		frame.setSize(600,600);
+		frame.setSize(350,150);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JLabel label = new JLabel("Welcome to the Bombermen Minesweeper Game!");
 		JPanel panel = new JPanel();
 		frame.add(panel);
 		panel.add(label);
-		
 		JButton button = new JButton("Play Minesweeper ");
 		panel.add(button);
-		panel.add(button);
-	}
-
+		button.addActionListener(new Action());
 }
+		static class Action implements ActionListener{
+			public void actionPerformed (ActionEvent e){
+				
+				JFrame frame2 = new JFrame("Selected Play");
+				frame2.setVisible(true);
+				frame2.setSize(600,600);
+				JLabel label = new JLabel("Play");
+				JPanel panel = new JPanel();
+				frame2.add(panel);
+				panel.add(label);
+				
+				}
+			}
+
+		}
+	
