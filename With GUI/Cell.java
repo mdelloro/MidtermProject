5d0d74e79d4e9d6b612a6733bd372b7497a0568b
Chris Susan <chriscsusan@gmail.com>
2016-09-20 18:06:37 -0400

    Test to add new folder with GUI version of project.

---
+++ With GUI/Cell.java
@@ -0,0 +1,69 @@
+package midterm;
+
+public class Cell {
+
+		private CellState state;
+		private Choosable isRevealed;
+		private Flagged flag;
+		int[] position = {0,0};
+		char stateDisplay;
+		
+		public Cell(int v, int h){
+			position[0] = v;
+			position[1] = h;
+			isRevealed = Choosable.NOT_REVEALED;
+			flag = Flagged.NOT_FLAGGED;
+		}
+		
+		public void setAsMine(){
+			state = CellState.MINE;
+			stateDisplay = '*';
+			
+		}
+		
+		public String getStateDisplay(){
+			return String.valueOf(stateDisplay);
+		}
+		
+		public void setAsNumber(int i){
+			state = CellState.NUMBER;
+			stateDisplay = (char)(i+48);
+		}
+		
+		public void setAsBlank(){
+			state = CellState.BLANK;
+			stateDisplay = ' ';
+		}
+		
+		public CellState getState(){
+			return state;
+		}
+		
+		
+		
+		public void setFlag(){
+			flag = Flagged.FLAGGED;
+		}
+		public void unsetFlag(){
+			flag = Flagged.NOT_FLAGGED;
+		}
+		
+		public boolean isFlagged(){
+			if (flag == Flagged.FLAGGED){
+				return true;
+			}
+			return false;
+		}
+		
+		public void setAsRevealed(){
+			isRevealed = Choosable.REVEALED;
+		}
+		
+		public void setAsJustRevealed(){
+			isRevealed = Choosable.JUST_REVEALED;
+		}
+		
+		public Choosable getChoosableState(){
+			return isRevealed;
+		}
+}
