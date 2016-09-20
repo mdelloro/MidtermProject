3b760c9c718c03cf17ee57bc48ea0585cb921ff5
mdelloro <m.delloro@yahoo.com>
2016-09-20 16:48:31 -0400

    Added all 8 tests

--- src/midterm/MinesweeperTest.java
+++ src/midterm/MinesweeperTest.java
@@ -7,7 +7,7 @@ import junit.framework.Assert;
 public class MinesweeperTest {
 	
 	@Test
-	public void testBlank(){
+	public void testForABlank(){
 		HiddenMinefield test = new HiddenMinefield(10, 10, 15);
 		test.setNumbers();
 		Assert.assertEquals(CellState.BLANK, test.board[5][5].getState());
@@ -28,7 +28,7 @@ public class MinesweeperTest {
 		Assert.assertEquals(Choosable.REVEALED, test.board[5][5].getChoosableState());
 	}
 	@Test
-	public void checkMines(){
+	public void testForAMine(){
 		HiddenMinefield test = new HiddenMinefield(10, 10, 99);
 		Minefield test2 = new Minefield (11, 11);
 		test.placeMines(4,4);
@@ -36,7 +36,7 @@ public class MinesweeperTest {
 		Assert.assertEquals(CellState.MINE, test.board[5][5].getState());
 	}
 	@Test
-	public void testAnotherMethod(){
+	public void testForANumber(){
 		HiddenMinefield test = new HiddenMinefield(10, 10, 99);
 			test.board[2][2].setAsMine();
 			test.board[2][3].setAsMine();
@@ -45,11 +45,31 @@ public class MinesweeperTest {
 			Assert.assertEquals('2',test.board[1][2].stateDisplay);
 			
 		}
-		
-	
-	
-	
+	@Test
+	public void testForFlag(){
+		HiddenMinefield test = new HiddenMinefield(10, 10, 20);
+		Minefield test2 = new Minefield (11, 11);
+		test.board[4][4].setFlag();
+		Assert.assertEquals(true, test.board[4][4].isFlagged());
 	}
+	@Test
+	public void testForUnflag(){
+		HiddenMinefield test = new HiddenMinefield(10, 10, 20);
+		Minefield test2 = new Minefield (11, 11);
+		test.board[4][4].setFlag();
+		test.board[4][4].unsetFlag();
+		Assert.assertEquals(false, test.board[4][4].isFlagged());
+		
+	}
+	@Test
+	public void testForABigFieldMine(){
+		HiddenMinefield test = new HiddenMinefield(100, 100, 9999);
+		Minefield test2 = new Minefield (11, 11);
+		test.placeMines(4,4);
+		test2.playerMove(5,5, test);
+		Assert.assertEquals(CellState.MINE, test.board[5][5].getState());
+	}
+}
 	
 
 
