e4236545f3a6e753115ec50ad3e5e3fd7613527e
Jhobson313 <james-hobson@att.net>
2016-09-20 14:59:05 -0400

    Working grid of buttons. Click makes button disables and changes text

--- src/midterm/Grid.java
+++ src/midterm/Grid.java
@@ -1,75 +1,76 @@
 package midterm;
-
 import java.awt.BorderLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
-public class Grid extends Cell {
+public class Grid implements ActionListener{
+	
+	JFrame frame = new JFrame("The Bombermen Minesweeper Game");
+	JButton reset = new JButton("Play Again");
+	JButton[][] buttons = new JButton[20][20];
+	int [][] counts = new int[20][20];
+	Container grid = new Container();
+	
+	public Grid(){
+	frame.setSize(450,550);
+	frame.setLayout(new BorderLayout());
+	frame.add(reset, BorderLayout.NORTH);
+	reset.addActionListener(this);
+	grid.setLayout(new GridLayout(20,20));
+	for(int a = 0; a < buttons.length; a++){
+		for(int b = 0; b < buttons.length; b++){
+			buttons[a][b] = new JButton();
+			buttons[a][b].addActionListener(this); 
+			grid.add(buttons[a][b]);
+			
+		}
+	}
+	frame.add(grid, BorderLayout.CENTER);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+	
+	
+	}
+	public void actionPerformed(){
+		
+		
+//		if (hiddenBoard.board[v][h].getState()==CellState.BLANK){
+//			grid[v+1][h+1].setEnabled(false);
+//			hiddenBoard.board[v][h].setAsJustRevealed();
+//			grid[v+1][h+1].setText(" ");
+//			hiddenBoard.checkForNearbyBlanks(v, h);
+//		}
+//		if (hiddenBoard.board[v][h].getState()==CellState.NUMBER){
+//			grid[v+1][h+1].setEnabled(false);
+//			grid[v+1][h+1].setText("1");
+//			hiddenBoard.board[v][h].setAsRevealed();
+//		}
 
-	public Grid(int v, int h) {
-		super(v, h);
+		
 		
 	}
-	public static void main(String[] args) {
-		HiddenMinefield hiddenBoard = new HiddenMinefield(5,5,5);
-		
-		JFrame frame = new JFrame("Bombermen Minesweeper Game");
-		frame.setVisible(true);
-		frame.setSize(350,150);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JLabel label = new JLabel("Welcome to the Bombermen Minesweeper Game!");
-		JPanel panel = new JPanel();
-		frame.add(panel);
-		panel.add(label);
-		JButton button = new JButton("Play Minesweeper ");
-		panel.add(button);
-		button.addActionListener(new Action());
-}
-		static class Action implements ActionListener{
-			public void actionPerformed (ActionEvent e){
-				
-				JFrame frame2 = new JFrame("Bombermen Minesweeper Game");
-				frame2.setVisible(true);
-				frame2.setSize(600,600);
-				frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				
-				
-				JPanel panel = new JPanel(new GridBagLayout());
-				frame2.getContentPane().add(panel,BorderLayout.NORTH);
-				GridBagConstraints c = new GridBagConstraints();
-				JLabel label01 = new JLabel("Welcome ");
-				c.gridx = 0;
-				c.gridy = 1;
-				c.insets = new Insets(10,10,10,10);
-				panel.add(label01,c);
-				JLabel label02 = new JLabel("To the");
-				c.gridx = 0;
-				c.gridy = 2;
-				c.insets = new Insets(10,10,10,10);
-				panel.add(label02,c);
-				JLabel label03 = new JLabel("Bombermen");
-				c.gridx = 0;
-				c.gridy = 3;
-				c.insets = new Insets(10,10,10,10);
-				panel.add(label03,c);
-				JLabel label04 = new JLabel("MineSweeper Game!");
-				c.gridx = 0;
-				c.gridy = 4;
-				c.insets = new Insets(10,10,10,10);
-				panel.add(label04,c);
-				
-				
-				}
-			}
-
-		}
 	
+	public static void main(String[] args) {
+		HiddenMinefield hidden = new HiddenMinefield(5,5,1);
+		new Grid();
+		
+				
+
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton button = (JButton) e.getSource();
+		button.setText("!");
+		button.setEnabled(false);
+		
+		
+	}
+
+}
